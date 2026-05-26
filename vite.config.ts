be641b2d1a967db3139fa8055b871b1b59d37d5f
christopherjoan <fernandochristopher33@gmail.com>
2026-05-26 08:56:22 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/designscape-studio/', // <-- SUDAH GUA TAMBAHIN DI SINI BRO
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
